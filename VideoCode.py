9f66c5ae5b95ffd0a18953191218c7ddc6cf2312
Brie Morris <morris.brie@gmail.com>
2024-02-07 10:41:40 -0600

    intiating dll

--- VideoCode.py
+++ VideoCode.py
@@ -1,46 +1,59 @@
-class Node:
-  def _init_(self, data):
-   self.data = data
-   self.nest = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
-class SinglyLinkedList:
-  def _init_(self):
-    self.head = None
-  
-  def add_node(self, data):
-    new_node = Node(data)
-    if self.head is None:
-      self.head = new_node
-    else:
-      current_node = self.head
-      while current_node.next:
-        current_node = current_node.next
-      current_node.next = new_node
 
-# display operation lets us know that our add_node operation is working 
-  def display(self):
-    current_node = self.head
-    while current_node:
-      print(current_node.data, ends=" ->")
-      current_node = current_node.next
-    print("None")
+# class SinglyLinkedList:
+#     def __init__(self):
+#         self.head = None  
 
-# Example Usage:
-    s11 = SinglyLinkedList()
-    s11.add.node(1)
-    s11.add.node(2)
-    s11.add.node(3)
-    s11.display()
+#     def add_node(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             current_node = self.head
+#             while current_node.next:
+#                 current_node = current_node.next
+#             current_node.next = new_node
+            
+#     def display(self):
+#         current_node = self.head
+#         while current_node:
+#             print(current_node.data, end=" -> ")
+#             current_node = current_node.next
+#         print("None")
+    
+#     def reverse(self):
+#         reversed_part = None
+#         current = self.head
+#         while current:
+#             future = current.next
+#             current.next = reversed_part
+#             reversed_part = current
+#             current = future
+        
+#         return reversed_part
 
+
+# # Example usage:
+# sll = SinglyLinkedList()
+# sll.add_node(1)
+# sll.add_node(2)
+# sll.add_node(3)
+# sll.display()
+# sll.reverse()
+# sll.display()
 
 class DoublyNode:
-  def _init_(self, data):
+  def __init__(self, data):
     self.data = data
     self.next = None
     self.prev = None
 
 class DoublyLinkedList:
-  def _init_(self):
+  def __init__(self):
     self.head = None
   
   def add_node(self, data):
@@ -54,29 +67,29 @@ class DoublyLinkedList:
       new_node.prev = current_node
   
   def display_forward(self):
-    current_node + self.head
+    current_node = self.head
     while current_node:
-      print(current_node.data, end="<->")
-      current_node= current_node.nest
+      print(current_node.data, end=" <-> ")
+      current_node = current_node.next
     print("None")
 
   def display_backward(self):
-    current_node = self.hand
+    current_node = self.head
     while current_node.next:
       current_node = current_node.next
     while current_node:
-      print(current_node.data, end="<->")
-      cureent_node = current_node.prev
+      print(current_node.data, end=" <-> ")
+      current_node = current_node.prev
     print("None")
 
 # Instantiate a new Doubly linked list
- d11 =DoublyLinkedList()   
+dll = DoublyLinkedList()   
 
 # add nodes to the list
-d11.add_node(1)
-d11.add_node(2)
-d11.add_node(3)
+dll.add_node(1)
+dll.add_node(2)
+dll.add_node(3)
     
 # Display the list in forward and backward direction
-d11.display_forward
-d11.display_backward
+dll.display_forward()
+dll.display_backward()
